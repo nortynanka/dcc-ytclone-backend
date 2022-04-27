8ae65c53787e9ef54fd8d2c164098497067e7d8e
Kaylee Guise <guisekayl@gmail.com>
2022-04-27 16:20:12 -0500

    Added GET all comments route.

--- routes/comments.js
+++ routes/comments.js
@@ -4,10 +4,32 @@ const { Comment, validateComment } = require("../models/comment");
 const { Reply, validateReply } = require("../models/reply");
 
 // ! GET ALL COMMENTS (10 min)
+// http://localhost:3003/api/comments
+router.get("/", async (req, res) => {
+    try {
+        let comments = await Comment.find();
+        if (!comments)
+            return res.status(400).send(`No comments in this collection.`);
+        return res.status(200).send(products);
+    } catch (error) {
+        return res.status(500).send(`Internal Server Error: ${error}`);
+    }
+});
+
+
 // ! GET A COMMENT BY ID (10 min)
+// http://localhost:3003/api/comments/:id
+
 // ! POST NEW COMMENT TO COMMENTS (20-30 min)
+// http://localhost:3003/api/comments/
+
 // ! UPDATE AN EXISTING LIKES / DISLIKES FOR COMMENT BY ID (20-30 min)
+// http://localhost:3003/api/comments/:id
+
 // ! DELETE A COMMENT BY ID (10 min)
+// http://localhost:3003/api/comments/:id
+
 // ! POST NEW REPLY TO COMMENT BY COMMENT ID (30-40 min)
+// http://localhost:3003/api/comments/:commentid
 
 module.exports = router;
