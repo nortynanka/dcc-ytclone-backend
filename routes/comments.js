53088498442cbead0a7f3262ad589dd4d19c95af
Kaylee Guise <guisekayl@gmail.com>
2022-04-28 14:31:56 -0500

    Added DELETE, PUT endpoints for comments.

--- routes/comments.js
+++ routes/comments.js
@@ -31,7 +31,7 @@ router.get("/:commentId", async (req, res) => {
 });
 
 // ! POST NEW COMMENT TO COMMENTS (20-30 min)
-// http://localhost:3003/api/comments/:videoid
+// http://localhost:3003/api/comments
 router.post("/", async (req, res) => {
   try {
     const { error } = validateComment(req.body);
@@ -48,11 +48,54 @@ router.post("/", async (req, res) => {
 
 // ! UPDATE AN EXISTING LIKES / DISLIKES FOR COMMENT BY ID (20-30 min)
 // http://localhost:3003/api/comments/:commentid
+router.put("/:commentId", async (req, res) => {
+  try {
+    const { error } = validateComment(req.body);
+    if (error) return res.status(400).send(error);
+    let comment = await Comment.findByIdAndUpdate(
+      req.params.productId,
+      req.body,
+      { new: true }
+    );
+    if (!comment)
+      return res
+        .status(400)
+        .send(`Comment with ID ${req.params.commentId} does not exist!`);
+    return res.send(comment);
+  } catch (error) {
+    return res.status(500).send(`Internal Server Error: ${error}`);
+  }
+});
 
 // ! DELETE A COMMENT BY ID (10 min)
 // http://localhost:3003/api/comments/:commentid
+router.delete("/:commentId", async (req, res) => {
+  try {
+    let comment = await Comment.findByIdAndDelete(req.params.commentId);
+    if (!comment)
+      return res
+        .status(400)
+        .send(`Comment with ID ${req.params.commentId} does not exist!`);
+    return res.send(comment);
+  } catch (error) {
+    return res.status(500).send(`Internal Server Error: ${error}`);
+  }
+});
 
 // ! POST NEW REPLY TO COMMENT BY COMMENT ID (30-40 min)
 // http://localhost:3003/api/comments/:commentid
+router.post("/:commentId", async (req, res) => {
+  try {
+    const { error } = validateReply(req.body);
+    if (error) return res.status(400).send(error);
+
+    let newReply = await new Reply(req.body);
+    await newReply.save();
+
+    return res.status(201).send(newReply);
+  } catch (error) {
+    return res.status(500).send(`Internal Server Error: ${error}`);
+  }
+});
 
 module.exports = router;
